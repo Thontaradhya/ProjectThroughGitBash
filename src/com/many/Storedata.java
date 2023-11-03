673b38c82f9cb483d52c1587def4da3ce447888a
devopscloud <tradhya691@gmail.com>
2023-11-03 09:07:09 +0530

    initial commit

---
+++ src/com/many/Storedata.java
@@ -0,0 +1,59 @@
+package com.many;
+
+import java.util.ArrayList;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Storedata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SessionFactory factory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		Session session=factory.openSession();  
+	      
+	    Transaction t=session.beginTransaction(); 
+	    
+	    Answer ans1=new Answer();  
+	    ans1.setAnswername("Java is a programming language");  
+	    ans1.setPostedBy("arun ");  
+	      
+	    Answer ans2=new Answer();  
+	    ans2.setAnswername("Java is a platform");  
+	    ans2.setPostedBy("sharath Kumar");  
+	     
+	    Question q1=new Question();  
+	    q1.setQname("What is Java?");  
+	    
+	    ArrayList<Answer> l1=new ArrayList<Answer>();  
+	    l1.add(ans1);  
+	    l1.add(ans2);  
+	    q1.setAnswers(l1);  
+	      
+	    Answer ans3=new Answer();    
+	    ans3.setAnswername("Servlet is an Interface");    
+	    ans3.setPostedBy("JaiKumar");    
+	            
+	    Answer ans4=new Answer();    
+	    ans4.setAnswername("Servlet is an API");    
+	    ans4.setPostedBy("Arun");    
+	      
+	    Question q2=new Question();  
+	    q2.setQname("What is Servlet?");  
+	    ArrayList<Answer> l2=new ArrayList<Answer>();  
+	    l2.add(ans3);  
+	    l2.add(ans4);  
+	    q2.setAnswers(l2);  
+	    session.persist(q1);    
+	    session.persist(q2);    
+	        
+	    t.commit();    
+	    session.close();    
+	    System.out.println("success");        
+	    
+
+	}
+
+}
